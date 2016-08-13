e91f23612b755411e5acaade748afe6b7af12912
Clayton G. Hobbs <clay@lakeserv.net>
2016-08-13 18:40:54 -0400

    Added initial MicroPython port of PyPNG
    Validating CRCs is not done, nor is any writing functionality; these are
    both due mainly to missing functions from MicroPython's uzlib module.

---
+++ setup.py
@@ -0,0 +1,20 @@
+import sys
+# Remove current dir from sys.path, otherwise setuptools will peek up our
+# module instead of system.
+sys.path.pop(0)
+from setuptools import setup
+
+
+setup(name='micropython-png',
+      version='0.0.1',
+      description='Minimal port of PyPNG for MicroPython',
+      long_description="A pure-MicroPython implementation of a PNG reader, "
+          "adapted from PyPNG.  The PNG writing functionality from PyPNG is "
+          "not implemented because of MicroPython's lack of support for zlib "
+          "compression.",
+      url='https://github.com/Ratfink/micropython-png',
+      author='Clayton G. Hobbs',
+      author_email = "clay@lakeserv.net",
+      license='MIT',
+      py_modules=['png'],
+      install_requires=['micropython-functools', 'micropython-itertools', 'micropython-operator', 'micropython-warnings'])
